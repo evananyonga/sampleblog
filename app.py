0caaa87c898832d019f3846749842a6105d7e44b
evananyonga <evewish@gmail.com>
2021-05-20 08:20:14 +0300

    add data storage area

--- app.py
+++ app.py
@@ -4,22 +4,25 @@ from werkzeug.wrappers import Request, Response
 from werkzeug.middleware.shared_data import SharedDataMiddleware
 from jinja2 import Environment, FileSystemLoader
 from werkzeug.routing import Rule, Map
-from werkzeug.exceptions import HTTPException
+from werkzeug.exceptions import HTTPException, NotFound
+from redis import StrictRedis
 
 
 class BlogApp(object):
     """Implements a WSGI application for managing your posts."""
 
-    def __init__(self):
-        """Initializes the Jinja templating engine to render from the 'templates' folder."""
+    def __init__(self, config):
+        """Initializes the Jinja templating engine to render from the 'templates' folder, defines the mapping of URLs to view methods, and initializes the Redis interface."""
         template_path = os.path.join(os.path.dirname(__file__), 'templates')
         self.jinja_env = Environment(loader=FileSystemLoader(template_path),
                                      autoescape=True)
 
         self.url_map = Map([
             Rule('/', endpoint='index'),
-            Rule('/register', endpoint='register'),
+            Rule('/register', endpoint='register', methods=['POST']),
+            Rule('/create', endpoint='add_post', methods=['GET', 'POST']),
         ])
+        self.redis = StrictRedis(config['redis_host'], config['redis_port'], decode_responses=True)
 
     def render_template(self, template_name, **context):
         """Renders the specified template file using the Jinja templating engine."""
@@ -38,9 +41,15 @@ class BlogApp(object):
         try:
             endpoint, values = adapter.match()
             return getattr(self, endpoint)(request, **values)
+        except NotFound:
+            return self.error_4o4
         except HTTPException as e:
             return e
 
+        # def error_404(self):
+        #     response = self.render_template("404.html")
+        #     response.status_code = 404
+        #     return response
 
     def wsgi_app(self, environ, start_response):
         """WSGI application that processes requests and returns responses."""
@@ -55,7 +64,7 @@ class BlogApp(object):
 
 def create_app():
     """Application factory function that returns an instance of an BlogApp."""
-    app = BlogApp()
+    app = BlogApp({'redis_host': 'localhost', 'redis_port': 6379})
     app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
         '/static': os.path.join(os.path.dirname(__file__), 'static')
     })
